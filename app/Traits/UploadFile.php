e25ec20a799dfb3865905ec98c54faabe7b601c1
mas-nano <nabilislam8@gmail.com>
2026-06-09 09:06:37 +0700

    set default disk for upload file

--- app/Traits/UploadFile.php
+++ app/Traits/UploadFile.php
@@ -2,16 +2,15 @@
 
 namespace App\Traits;
 
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
-
 
 trait UploadFile
 {
-    public function upload($path, $file, $disk = null): string
+    public function upload($path, $file, $disk = 'public'): string
     {
         $name = explode('.', $file->getClientOriginalName());
-        $fullName = Str::replace(' ', '_', $name[0]) . '_' . Str::random(5) . '.' . $file->getClientOriginalExtension();
+        $fullName = Str::replace(' ', '_', $name[0]).'_'.Str::random(5).'.'.$file->getClientOriginalExtension();
+
         return $file->storeAs($path, $fullName, $disk);
     }
 }
