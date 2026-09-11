6495a466452664ed3f841156eb6adc79709c53a1
mas-nano <nabilislam8@gmail.com>
2026-09-11 09:04:29 +0700

    fix: set default disk for file upload to s3

--- app/Traits/UploadFile.php
+++ app/Traits/UploadFile.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Str;
 
 trait UploadFile
 {
-    public function upload($path, $file, $disk = 'public'): string
+    public function upload($path, $file, $disk = 's3'): string
     {
         $name = explode('.', $file->getClientOriginalName());
         $fullName = Str::replace(' ', '_', $name[0]).'_'.Str::random(5).'.'.$file->getClientOriginalExtension();
